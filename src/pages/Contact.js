af942f7fa013e8fc86f408368f1405065cd10956
kostalexis89 <kostalexis.hosting@gmail.com>
2022-01-10 17:29:30 +0100

    contact added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "../styles/contact.scss";
 
 export default function Contact() {
-    return (
-        <div>
-            contact
-        </div>
-    )
+  return (
+    <div className="contact-view">
+      <h1>Feel free to contact me</h1>
+      <div className="contact-view__ways">
+        <button
+          className="sides-nav__item"
+          onClick={() => (window.location = "mailto:yourmail@gmail.com")}
+        >
+          e-mail
+        </button>
+
+        <a
+          href="https://www.linkedin.com/in/konstantinosalexis/"
+          className="sides-nav__item"
+          rel="noreferrer"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+        <span>+4915751115057</span>
+      </div>
+    </div>
+  );
 }
