437353f738747cf62dcaa033694c02cfcf92879b
kostalexis89 <kostalexis.hosting@gmail.com>
2022-01-11 15:43:59 +0100

    added wordress projects

--- src/components/Wordpress.js
+++ src/components/Wordpress.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "../styles/projectComponentBox.scss"
 
 export default function Wordpress() {
-    return (
-        <div className='container-contribution'>
-        <h1>Under Construction</h1>
-    </div>
-    )
+  return (
+    <section className="section-features">
+      <a href="https://horostheatre.gr/" rel="noreferrer" target="_blank">
+        <div className="project-box">
+          <img className="icon" alt="react" src="https://horostheatre.gr/wp-content/uploads/2021/01/HOROS-LOGO-identity.png"></img>
+          <p>Horos Theatre Company</p>
+          <div className="stack">
+            <p>Wordpress,</p>
+            <p>Divi,</p>
+            <p>ACF</p>
+          </div>
+        </div>
+      </a>
+
+      <a href="https://www.bullicoffee.de/" rel="noreferrer" target="_blank">
+        <div className="project-box">
+          <img src="https://www.bullicoffee.de/wp-content/uploads/2021/01/Logo-Bulli1238.png" className="icon" alt="react"></img>
+          <p>BulliCoffee Van</p>
+          <div className="stack">
+            <p>Wordpress,</p>
+            <p>Divi,</p>
+            <p>Yoast</p>
+          </div>
+        </div>
+      </a>
+      
+    </section>
+  );
 }
